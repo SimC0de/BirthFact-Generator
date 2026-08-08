df03b1ff3d1cc0f7c4878b7d86da64e71af6dc89
SimC0de <yor1simone@outlook.com>
2026-08-08 21:23:31 +0800

    Shows what Chineze Zodiac Sign the user is

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 // import that makes the user prompt an input
 const prompt = require('prompt-sync')();
 
-const months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
+const currentYear = new Date().getFullYear();
+const months = ["january", "february", "march", "april", "may", "june",
+    "july", "august", "september", "october", "november", "december"];
+
+let birthYear = Number(prompt('Enter your birth year:'));
+let userAge = currentYear - birthYear;
+
+if (userAge <= 12 || userAge >= 80) {
+    console.log("Not gonna lie, your birth year is not within the age demographic that this project is targeting... but I'll let you keep using it");
+}
 
 // prompts the user to enter their birth month by name or number
 let birthMonth = prompt('Enter your birth month by month name (e.g."June") or month number (e.g. "6"):').toLowerCase();
@@ -14,18 +23,33 @@ while (!validMonth) {
     if (Number.isNaN(parsedMonth)) {
         // checks if string is a valid month name
         if (!months.includes(birthMonth)) {
-            birthMonth = prompt('Not a valid month name, please type the full name of the month or use a valid month number:');
+            birthMonth = prompt('Not a valid month name, please type the full name of the month or use a valid month number:').toLowerCase();
         } else {
             validMonth = true;
         }
     } else {
         // checks if number is within 12 number of months
         if (months[birthMonth - 1] === undefined) {
-            birthMonth = prompt('Not a valid month number, please type a valid month number or use the full name of the month:');
+            birthMonth = prompt('Not a valid month number, please type a valid month number or use the full name of the month:').toLowerCase();
         } else {
             birthMonth = months[birthMonth - 1];
             validMonth = true;
         }
     }
 }
+
+function chineseZodiac(year) {
+    const animals = [
+        "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake",
+        "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig"
+    ];
+
+    const index = (year - 4) % 12;
+    const animal = animals[index < 0 ? index + 12 : index];
+    return `Your Chinese Zodiac sign is year of the ${animal}`;
+}
+
+const randomFact = [chineseZodiac(birthYear)];
+
+console.log(randomFact[Math.floor(Math.random() * randomFact.length)]);
 
