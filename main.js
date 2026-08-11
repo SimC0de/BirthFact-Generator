1611bc6eb07b7b0e9a51556b8fb9976f4cb44691
SimC0de <yor1simone@outlook.com>
2026-08-11 13:39:48 +0800

    Checks what your Zodiac star sign is

--- main.js
+++ main.js
@@ -54,6 +54,41 @@ while (!validMonthDay) {
     }
 }
 
+function zodiacConstellation(month, day) {
+    const constellations = [
+        "Aries", "Taurus", "Gemini", "Cancer",
+        "Leo", "Virgo", "Libra", "Scorpius",
+        "Sagittarius", "Capricornus", "Aquarius", "Pisces"]
+    let constellation = "";
+    if ((month === 3 && day >= 21) || (month === 4 && day <= 19)) {
+        constellation = "Aries";
+    } else if ((month === 4 && day >= 20) || (month === 5 && day <= 20)) {
+        constellation = "Taurus";
+    } else if ((month === 5 && day >= 21) || (month === 6 && day <= 21)) {
+        constellation = "Gemini";
+    } else if ((month === 6 && day >= 22) || (month === 7 && day <= 22)) {
+        constellation = "Cancer";
+    } else if ((month === 7 && day >= 23) || (month === 8 && day <= 22)) {
+        constellation = "Leo";
+    } else if ((month === 8 && day >= 23) || (month === 9 && day <= 22)) {
+        constellation = "Virgo";
+    } else if ((month === 9 && day >= 23) || (month === 10 && day <= 23)) {
+        constellation = "Libra";
+    } else if ((month === 10 && day >= 24) || (month === 11 && day <= 21)) {
+        constellation = "Scorpio";
+    } else if ((month === 11 && day >= 22) || (month === 12 && day <= 21)) {
+        constellation = "Sagittarius";
+    } else if ((month === 12 && day >= 22) || (month === 1 && day <= 19)) {
+        constellation = "Capricorn";
+    } else if ((month === 1 && day >= 20) || (month === 2 && day <= 18)) {
+        constellation = "Aquarius";
+    } else if ((month === 2 && day >= 19) || (month === 3 && day <= 20)) {
+        constellation = "Pisces";
+    }
+
+    return `Your Zoadiac Sign is ${constellation}`;
+}
+
 function chineseZodiac(year) {
     const animals = [
         "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake",
@@ -67,7 +102,8 @@ function chineseZodiac(year) {
 
 
 
-const randomFact = [chineseZodiac(birthYear)];
+const randomFact = [chineseZodiac(birthYear),];
 
 console.log(randomFact[Math.floor(Math.random() * randomFact.length)]);
+console.log(zodiacConstellation(birthMonth, birthDay));
 
